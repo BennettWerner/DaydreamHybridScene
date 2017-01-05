fcf673fb511370e81010b6c05b7d1cec984bef39
Simon Schliesky <simon.schliesky@kaasahealth.com>
2017-01-05 13:22:07 +0100

    Initial commit

---
+++ Assets/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+	public enum VRMode 
+	{ 	
+		VROn,
+		VROff
+	}
+	VRMode vrMode;
+	// Use this for initialization
+	void Awake () 
+	{
+		vrMode = VRMode.VROff;
+		DontDestroyOnLoad(gameObject);
+		InvokeRepeating("toggle", 5f, 5f);
+	}
+
+	void toggle() 
+	{
+		if (vrMode == VRMode.VROn)
+		{
+			Debug.Log("Starting Non-VR");
+			VRSettings.LoadDeviceByName("None");
+			vrMode = VRMode.VROff;
+		}
+		else
+		{ 
+			Debug.Log("Starting VR");
+			VRSettings.LoadDeviceByName("daydream");
+			vrMode = VRMode.VROn;
+		}
+	}
+}
